9b8ce2cb535245e927b2a10e6080374bd42b0ae8
ANORA\prabhakar <Anora>
2024-03-30 14:24:10 +0530

    added command to sturcture file

--- 01_structure/01_structure/main.c
+++ 01_structure/01_structure/main.c
@@ -13,6 +13,7 @@ struct Person
 int main(void)
 {
     struct Person p[5]={{"Prabakar",21,'M',162,65.0},{"Ankur",22,'M',170,70.0},{"Mohan",21,'M',162,60.0},{"Jothi",21,'F',162,60.0},{"Sethulakshmi",22,'F',162,60.0}};
+	//Command added
 	return 0;
 }
 
